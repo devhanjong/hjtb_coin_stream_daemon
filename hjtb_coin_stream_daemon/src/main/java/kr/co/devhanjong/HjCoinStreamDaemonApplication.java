3e853c3e3278574534a09a011551615ef78b5997
jhhan <jhhan@upchain.kr>
2023-07-03 13:22:53 +0900

    commit

--- hjtb_coin_stream_daemon/src/main/java/kr/co/devhanjong/HjCoinStreamDaemonApplication.java
+++ hjtb_coin_stream_daemon/src/main/java/kr/co/devhanjong/HjCoinStreamDaemonApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class UpCoinStreamDaemonApplication {
+public class HjCoinStreamDaemonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UpCoinStreamDaemonApplication.class, args);
+        SpringApplication.run(HjCoinStreamDaemonApplication.class, args);
     }
 
 }
